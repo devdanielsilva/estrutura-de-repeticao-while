8fab4e94d76d4e9a535e1e6f68999b52b744bc44
Daniel Silva <devdanielsilva@gmail.com>
2023-11-08 16:58:45 -0300

    Create estrtura-while.php

---
+++ estrtura-while.php
@@ -0,0 +1,7 @@
+<?php /*Contagem de 1 até 10. */
+$c = 1;
+while($c >= 10){
+    echo $c . "<br>";
+    $c ++; //Variável c irá receber 1 número mais o número que já está dentro da variável. EX: c = 1 recebe +1 e fica variável c = 2 
+}
+ ?>
